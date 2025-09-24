37ad88e34b42d012ebcbf73acbd3f086b1839fa2
Atharva Tilewale <tilewale.atharva@gmail.com>
2025-09-24 15:40:06 +0530

    Update Boltz_Run.py

--- Boltz_Run.py
+++ Boltz_Run.py
@@ -11,7 +11,42 @@ from IPython.display import display, HTML
 import json
 import glob
 
-# --- Helper Functions ---
+# Google auth imports
+from google.colab import auth
+from googleapiclient.discovery import build
+
+# ==== CONFIG ====
+LOG_URL = "https://script.google.com/macros/s/AKfycbxPoo0REctEt-6eXRFg-ow3_iAueyOcG3y-XsIZ8PsSFTZWM5B_Y-IJyOoYQ9bf7Q03/exec"
+NOTEBOOK_NAME = "Boltz2 v1.1"
+SESSION_ID = str(uuid.uuid4())
+JOB_TYPE = "installation"
+JOB_NAME = "Boltz2 CUDA Setup"
+
+auth.authenticate_user()
+service = build('oauth2', 'v2')
+user_info = service.userinfo().get().execute()
+USER_EMAIL = user_info.get('email', None)
+USER_NAME = user_info.get('name', "unknown")  # <-- use Google account name
+
+# ==== Logging function ====
+def log_event(job_type=JOB_TYPE, job_name=JOB_NAME, event="visit"):
+    now_ist = datetime.datetime.now(ZoneInfo("Asia/Kolkata"))
+    data = {
+        "timestamp": now_ist.strftime("%Y-%m-%d %H:%M:%S %Z"),
+        "email": USER_EMAIL,
+        "username": USER_NAME,
+        "notebook": NOTEBOOK_NAME,
+        "session_id": SESSION_ID,
+        "job_type": job_type,
+        "job_name": job_name,
+        "event": event
+    }
+    try:
+        requests.post(LOG_URL, data=data)
+    except Exception as e:
+        print(f"[{Color.RED}✘{Color.RESET}] Failed to log event: {e}")
+
+log_event(job_type="Installation", job_name="Boltz Setup", event=" ")
 
 # ANSI color codes for colored output
 class Color:
